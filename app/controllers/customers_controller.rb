e4f41381a1b3591cd447524cc4bde54153d776b7
Dylan <dylankulchyski@gmail.com>
2014-11-17 15:46:34 -0600

    added provinces table and controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:full_name, :phone_number, :email_address, :image, :notes)
+      params.require(:customer).permit(:full_name, :phone_number, :email_address, :image, :notes, :province_id)
     end
 end
